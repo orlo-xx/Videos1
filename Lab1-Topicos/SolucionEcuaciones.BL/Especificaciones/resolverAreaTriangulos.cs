a58cf4487db606e536cb3577449e85138ff034ab
kemeye16 <kemeye.16@gmail.com>
2015-11-07 19:58:15 -0600

    especificaciones

---
+++ Lab1-Topicos/SolucionEcuaciones.BL/Especificaciones/resolverAreaTriangulos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Figuras.DM;
+using SolucionEcuaciones.BL.Acciones;
+
+namespace SolucionEcuaciones.BL.Especificaciones
+{
+    public class resolverAreaTriangulos
+    {
+        public double resolverArea(triangulos tR)
+        {
+            //Validaciones
+            if ((tR.lado1 != 0 && tR.lado2 != 0 && tR.lado3 != 0) && (tR.lado1 <= (tR.lado2 + tR.lado3)) || (tR.lado2 <= (tR.lado1 + tR.lado3)) || (tR.lado3 <= (tR.lado2 + tR.lado1)))
+            {
+
+                calculoFormulaHeron calcArea = new calculoFormulaHeron();
+                    tR = calcArea.formulaHeron(tR);
+            }
+            else
+            {
+
+                Exception ex = new Exception(string.Format("Ocurrió un error al calcular el Area, verifica que:" +
+                    "Todos los lados son mayores a 0. \n La suma de 2 lados sea menor o igual al tercer lado."));
+                throw ex;
+            }
+            return tR.area;
+        }
+    }
+}
---
+++ Lab1-Topicos/SolucionEcuaciones.BL/Especificaciones/resolverPerimetroTriangulos.cs
@@ -0,0 +1,33 @@
+﻿using Figuras.DM;
+using SolucionEcuaciones.BL.Acciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SolucionEcuaciones.BL.Especificaciones
+{
+    class resolverPerimetroTriangulos
+    {
+        public double resolverArea(triangulos tR)
+        {
+            //Validaciones
+            if ((tR.lado1 != 0 && tR.lado2 != 0 && tR.lado3 != 0) && (tR.lado1 <= (tR.lado2 + tR.lado3)) || (tR.lado2 <= (tR.lado1 + tR.lado3)) || (tR.lado3 <= (tR.lado2 + tR.lado1)))
+            {
+
+                sumaLadosTriangulo calcPerimetro = new sumaLadosTriangulo();
+                tR = calcPerimetro.Suma(tR);
+            }
+            else
+            {
+
+                Exception ex = new Exception(string.Format("Ocurrió un error al calcular el Area, verifica que:" +
+                    "Todos los lados son mayores a 0. \n La suma de 2 lados sea menor o igual al tercer lado."));
+                throw ex;
+            }
+            return tR.perimetro;
+        }
+    }
+}
